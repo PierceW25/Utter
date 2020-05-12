551d1e344129bc09a6e9921cc7d25496fc79fab1
Me <wwwierschem@icloud.com>
2020-05-11 21:51:13 -0400

    Initial Commit

---
+++ UtterTests/UtterTests.swift
@@ -0,0 +1,34 @@
+//
+//  UtterTests.swift
+//  UtterTests
+//
+//  Created by Pierce Wierschem on 5/11/20.
+//  Copyright © 2020 Pierce Wierschem. All rights reserved.
+//
+
+import XCTest
+@testable import Utter
+
+class UtterTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
